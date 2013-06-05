7962e739b2148d0736aae9ea0ba1732888e021e2
Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2013-06-06 00:21:17 +0200

    create DB Connection (After run the app, you will have fsvtool.db.h2 file in your home directory) add EntityManager add User Classes add H2 library

--- src/fsvtool/FSVtool.java
+++ src/fsvtool/FSVtool.java
@@ -4,16 +4,35 @@
  */
 package fsvtool;
 
+import fsvtool.persistance.EntityManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Sadeq
  */
 public class FSVtool {
-
+    private static Connection connectDB() throws ClassNotFoundException, SQLException{
+        Class.forName("org.h2.Driver");
+        String dbUrl = "jdbc:h2:"+System.getProperty("user.home")+"/fsvtool";
+        System.out.print(dbUrl);
+        return DriverManager.getConnection(dbUrl);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Connection conn = FSVtool.connectDB();
+            EntityManager em = new EntityManager(conn);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.print("Kann keine Datenbankverbindung aufbauen\n"
+                    +"Exception message: "+e.getMessage()+"\n"
+                    +"Stack trace: ");
+            e.printStackTrace(System.err);
+        }
     }
 }
---
+++ src/fsvtool/persistance/UserProvider.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsvtool.persistance;
+
+/**
+ *
+ * @author ahmet
+ */
+public class UserProvider {
+    private EntityManager em;
+    
+    private String createSQL = "CREATE TABLE IF NOT EXISTS fsv_user ("+
+            " id INT AUTO_INCREMENT PRIMARY KEY,"+
+            " name VARCHAR(64) NOT NULL,"+
+            " vorname VARCHAR(64) NOT NULL,"+
+            " password VARCHAR(256) NOT NULL,"+
+            " phone_nr VARCHAR(20) NOT NULL,"+
+            " plz VARCHAR(5) NOT NULL "+
+            " ); \n"+
+            "CREATE TABLE IF NOT EXISTS fsv_user_skill ("+
+            " id INT AUTO_INCREMENT PRIMARY KEY,"+
+            " user_id INT, "+
+            " "+
+            " FOREIGN KEY(user_id) REFERENCES fsv_user(id) "+
+            " )";
+
+    public UserProvider(EntityManager em) {
+        this.em = em;
+    }
+    
+    public String getCreateSQL() {
+        return this.createSQL;
+    }
+}
