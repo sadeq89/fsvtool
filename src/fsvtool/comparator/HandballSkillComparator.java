87ad9460ff45f1d6105525f94a0df9a022684a36
Marce Pütz <marcelpuetz@web.de>
2013-06-25 13:43:50 +0200

    corrected comparator package name

---
+++ src/fsvtool/comparator/HandballSkillComparator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsvtool.comparator;
+
+import fsvtool.persistance.IUser;
+import java.util.Comparator;
+
+/**
+ *
+ * @author Marcel
+ */
+public class HandballSkillComparator implements Comparator{
+    @Override
+    public int compare(Object o1, Object o2) {
+        final int type = IUser.SKILL_TYPE_HANDBALL;
+          if (((IUser)o1).getSkill(type)>((IUser)o2).getSkill(type))
+              return 1;
+          else if (((IUser)o1).getSkill(type)<((IUser)o2).getSkill(type))
+              return -1;
+          else
+              return 0;
+    }
+}
