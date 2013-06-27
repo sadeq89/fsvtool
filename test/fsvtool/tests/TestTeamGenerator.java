674a14ca9690147daabab18c0de7486c3ad462ca
Marce Pütz <marcelpuetz@web.de>
2013-06-28 00:55:37 +0200

    Fixed TeamGenerator Test runns!!

--- test/fsvtool/tests/TestTeamGenerator.java
+++ test/fsvtool/tests/TestTeamGenerator.java
@@ -9,9 +9,6 @@ import fsvtool.persistance.IGame;
 import fsvtool.persistance.IUser;
 import fsvtool.persistance.TeamGenerator;
 import fsvtool.persistance.User;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -23,21 +20,38 @@ public class TestTeamGenerator {
 
     User[] user = new User[12];
     Game game1 = new Game();
+    Game game2 = new Game();
 
     @Test
     public void checkGenerateTeams(){
         initUser();
         
+        game1.setGameType(IGame.TYPE_SOCCER);
         game1.addPlayerToTeam(user[0], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[4], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[5], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[8], IGame.TEAM_NO_TEAM);
+        System.out.println("0112");
         
         TeamGenerator tg = new TeamGenerator();
         tg.generateTeams(game1);
         
         System.out.println("Team1: " + game1.getPlayerInTeam(1));
         System.out.println("Team2: " + game1.getPlayerInTeam(2));
+        
+        game2.setGameType(IGame.TYPE_SOCCER);
+        game2.addPlayerToTeam(user[0], IGame.TEAM_NO_TEAM);
+        game2.addPlayerToTeam(user[1], IGame.TEAM_NO_TEAM);
+        game2.addPlayerToTeam(user[4], IGame.TEAM_NO_TEAM);
+        game2.addPlayerToTeam(user[5], IGame.TEAM_NO_TEAM);
+        game2.addPlayerToTeam(user[8], IGame.TEAM_NO_TEAM);
+        game2.addPlayerToTeam(user[9], IGame.TEAM_NO_TEAM);
+        System.out.println("001122");
+        
+         tg.generateTeams(game2);
+         
+         System.out.println("Team1: " + game2.getPlayerInTeam(1));
+         System.out.println("Team2: " + game2.getPlayerInTeam(2));
     }
     
     private void initUser(){
@@ -46,13 +60,13 @@ public class TestTeamGenerator {
         }
         
         for(int i=0; i<4; i++){
-            user[i].setSkill(0, 0);
+            user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_BAD);
         }
         for(int i=4; i<8; i++){
-            user[i].setSkill(0, 1);
+            user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_MIDDLE);
         }
         for(int i=8; i<12; i++){
-            user[i].setSkill(0, 2);
+            user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_GREAT);
         }
         
     }
