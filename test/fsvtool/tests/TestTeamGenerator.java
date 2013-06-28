2fff0d79bd945ffe85382aed5b9141e3b361bd9e
Marce Pütz <marcelpuetz@web.de>
2013-06-28 14:52:05 +0200

    Expanded TestGenerator

--- test/fsvtool/tests/TestTeamGenerator.java
+++ test/fsvtool/tests/TestTeamGenerator.java
@@ -21,24 +21,25 @@ public class TestTeamGenerator {
     User[] user = new User[12];
     Game game1 = new Game();
     Game game2 = new Game();
+    Game game3 = new Game();
 
     @Test
-    public void checkGenerateTeams(){
+    public void checkGenerateTeams() {
         initUser();
-        
+
         game1.setGameType(IGame.TYPE_SOCCER);
         game1.addPlayerToTeam(user[0], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[4], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[5], IGame.TEAM_NO_TEAM);
         game1.addPlayerToTeam(user[8], IGame.TEAM_NO_TEAM);
-        System.out.println("0112");
-        
+        System.out.println(game1.getPlayerInTeam(IGame.TEAM_NO_TEAM));
+
         TeamGenerator tg = new TeamGenerator();
         tg.generateTeams(game1);
-        
+
         System.out.println("Team1: " + game1.getPlayerInTeam(1));
         System.out.println("Team2: " + game1.getPlayerInTeam(2));
-        
+
         game2.setGameType(IGame.TYPE_SOCCER);
         game2.addPlayerToTeam(user[0], IGame.TEAM_NO_TEAM);
         game2.addPlayerToTeam(user[1], IGame.TEAM_NO_TEAM);
@@ -46,28 +47,46 @@ public class TestTeamGenerator {
         game2.addPlayerToTeam(user[5], IGame.TEAM_NO_TEAM);
         game2.addPlayerToTeam(user[8], IGame.TEAM_NO_TEAM);
         game2.addPlayerToTeam(user[9], IGame.TEAM_NO_TEAM);
-        System.out.println("001122");
-        
-         tg.generateTeams(game2);
-         
-         System.out.println("Team1: " + game2.getPlayerInTeam(1));
-         System.out.println("Team2: " + game2.getPlayerInTeam(2));
+        System.out.println(game2.getPlayerInTeam(IGame.TEAM_NO_TEAM));
+
+        tg.generateTeams(game2);
+
+        System.out.println("Team1: " + game2.getPlayerInTeam(1));
+        System.out.println("Team2: " + game2.getPlayerInTeam(2));
+
+        game3.setGameType(IGame.TYPE_SOCCER);
+        game3.addPlayerToTeam(user[4], IGame.TEAM_NO_TEAM);
+        game3.addPlayerToTeam(user[10], IGame.TEAM_NO_TEAM);        
+        game3.addPlayerToTeam(user[8], IGame.TEAM_NO_TEAM);        
+        game3.addPlayerToTeam(user[5], IGame.TEAM_NO_TEAM);
+        game3.addPlayerToTeam(user[9], IGame.TEAM_NO_TEAM);       
+        game3.addPlayerToTeam(user[6], IGame.TEAM_NO_TEAM);
+        game3.addPlayerToTeam(user[0], IGame.TEAM_NO_TEAM);        
+        game3.addPlayerToTeam(user[7], IGame.TEAM_NO_TEAM);
+
+
+        System.out.println(game3.getPlayerInTeam(IGame.TEAM_NO_TEAM));
+
+        tg.generateTeams(game3);
+
+        System.out.println("Team1: " + game3.getPlayerInTeam(1));
+        System.out.println("Team2: " + game3.getPlayerInTeam(2));
     }
-    
-    private void initUser(){
-        for(int i=0; i<12; i++){
+
+    private void initUser() {
+        for (int i = 0; i < 12; i++) {
             user[i] = new User();
         }
-        
-        for(int i=0; i<4; i++){
+
+        for (int i = 0; i < 4; i++) {
             user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_BAD);
         }
-        for(int i=4; i<8; i++){
+        for (int i = 4; i < 8; i++) {
             user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_MIDDLE);
         }
-        for(int i=8; i<12; i++){
+        for (int i = 8; i < 12; i++) {
             user[i].setSkill(IGame.TYPE_SOCCER, IUser.SKILL_VALUE_GREAT);
         }
-        
+
     }
 }
