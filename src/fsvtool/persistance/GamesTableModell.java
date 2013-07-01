aa43a8673a1d732ff2915b71a2e1abbf14489fbd
Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2013-07-01 20:23:11 +0200

    Buxfixes in TableModel and GuiCreateGame

--- src/fsvtool/persistance/GamesTableModell.java
+++ src/fsvtool/persistance/GamesTableModell.java
@@ -4,6 +4,9 @@
  */
 package fsvtool.persistance;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
@@ -15,15 +18,19 @@ public class GamesTableModell extends AbstractTableModel {
 
     private GameProvider gp;
     private String[] columnNames = new String[]{
-        "Auswählen", "Anzahl Spieler", "Datum/Uhrzeit", "Ort", "Teilname"
+        "Sportart", "Anzahl Spieler", "Datum/Uhrzeit", "Ort", "Teilname"
     };
     protected Class<?>[] dataTypes = new Class[]{
-        Boolean.class, String.class, String.class, String.class, String.class, String.class
+        String.class, String.class, String.class, String.class, String.class, String.class
     };
     private List<IGame> list;
+    private final DateFormat dateFormat;
+    private final Calendar calendar;
 
     public GamesTableModell(GameProvider gp) {
         this.gp = gp;
+        this.dateFormat = new SimpleDateFormat("HH:mm dd.MM.yyyy");
+        this.calendar = Calendar.getInstance();
     }
 
     @Override
@@ -53,8 +60,15 @@ public class GamesTableModell extends AbstractTableModel {
         }
         switch (columnIndex) {
             case 0:
-                // Checkbox
-                return false;
+                // Sportart
+                switch(this.list.get(rowIndex).getGameType()) {
+                    case IGame.TYPE_VOLLEYBALL:
+                        return "Volleyball";
+                    case IGame.TYPE_SOCCER:
+                        return "Fußball";
+                    case IGame.TYPE_HANDBALL:
+                        return "Handball";
+                }
             case 1:
                 // Anzahl Spieler
                 return this.list.get(rowIndex).getPlayerInGameCount()
@@ -62,7 +76,12 @@ public class GamesTableModell extends AbstractTableModel {
                         + this.list.get(rowIndex).getMaxPlayerCount();
             case 2:
                 // Datum
-                return this.list.get(rowIndex).getDate();
+                this.calendar.setTime(this.list.get(rowIndex).getDate());
+                this.calendar.add(
+                    Calendar.MILLISECOND,
+                    (int) this.list.get(rowIndex).getTime().getTime()
+                );
+                return this.dateFormat.format(this.calendar.getTime());
             case 3:
                 // Ort
                 return this.list.get(rowIndex).getGameLocation();
@@ -83,8 +102,15 @@ public class GamesTableModell extends AbstractTableModel {
         return list.get(index);
     }
 
+    @Override
+    public void fireTableDataChanged() {
+        super.fireTableDataChanged();
+        list = gp.getAllGames();
+    }
+    
+    
+
     private void setValues() {
         list = gp.getAllGames();
-        System.out.println(list);
     }
 }
